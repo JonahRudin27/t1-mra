5e42e91157068dd3130abcbf1c84bb3be947a3d8
jonah rudin <jonaharudin@gmail.com>
2024-07-11 13:45:52 -0700

    all models training at once

--- src/train.py
+++ src/train.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     # setup perceptual loss
     feature_extractor = VGG16FeatureExtractor()
     feature_extractor.to(device)
-    perceptual_loss = PerceptualLoss(feature_extractor, MSELoss)
+    perceptual_loss = PerceptualLoss_3d(feature_extractor, MSELoss)
 
     # def transforms
     train_transform = v2.Compose([
@@ -101,78 +101,101 @@ if __name__ == "__main__":
     print(f"Current memory usage: {current_memory / (1024**3)} GB")
 
     # def model
-    model = UNet(1, 1)
+    z_model = UNet(1, 1)
+    y_model = UNet(1, 1)
+    x_model = UNet(1, 1)
 
-    if args.force_single_gpus:
-        gpu_count = 1
-    else:
-        gpu_count = torch.cuda.device_count()
-        print(f"Found {gpu_count} GPUs")
-        if gpu_count > 1:
-            model = torch.nn.DataParallel(model)
-            print(f"Using DataParallel with {gpu_count} GPUs")
+    models = [z_model, y_model, x_model]
 
-    model.to(device)
+    def config_model(model):
+        if args.force_single_gpus:
+            gpu_count = 1
+        else:
+            gpu_count = torch.cuda.device_count()
+            print(f"Found {gpu_count} GPUs")
+            if gpu_count > 1:
+                model = torch.nn.DataParallel(model)
+                print(f"Using DataParallel with {gpu_count} GPUs")
 
-    # def optimizer
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
-                                                     factor=0.5, patience=10)
-    num_epochs = args.num_epochs
+        model.to(device)
 
-    # load checkpoint if exists
-    if os.path.exists("model_checkpoint.pth"):
-        print("Model checkpoint found, loading")
-        checkpoint = torch.load("model_checkpoint.pth")
-        model.load_state_dict(checkpoint["model_state_dict"])
-        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-        start_epoch = checkpoint["epoch"]
-        print(f'Loaded model, starting from epoch {start_epoch}')
-    else:
-        start_epoch = 0
+        # def optimizer
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
+                                                        factor=0.5, patience=10)
+        num_epochs = args.num_epochs
 
-    print(f'Starting training for {num_epochs} epochs')
+        # load checkpoint if exists
+        if os.path.exists("model_checkpoint.pth"):
+            print("Model checkpoint found, loading")
+            checkpoint = torch.load("model_checkpoint.pth")
+            model.load_state_dict(checkpoint["model_state_dict"])
+            optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+            start_epoch = checkpoint["epoch"]
+            print(f'Loaded model, starting from epoch {start_epoch}')
+        else:
+            start_epoch = 0
+        
+        return optimizer, scheduler, start_epoch, num_epochs
+    
+    class TrainingSession:
+        def __init__(self, optimizer, scheduler, start_epoch, num_epochs):
+            self.optimizer = optimizer
+            self.scheduler = scheduler
+            self.start_epoch = start_epoch
+            self.num_epochs = num_epochs
+
+    training = [TrainingSession(config_model(z_model)), 
+                TrainingSession(config_model(y_model)), 
+                TrainingSession(config_model(x_model))]
+
+    print(f'Z model: Starting training for {training[0].num_epochs} epochs \n')
+    print(f'y model: Starting training for {training[1].num_epochs} epochs \n')
+    print(f'x model: Starting training for {training[2].num_epochs} epochs \n')
     # training loop
-    for epoch in range(start_epoch, num_epochs):
-        train_loss = train_scans(model, train_dataloader, PerceptualLoss_3d.get_loss,
-                           optimizer, device)
-        val_loss = validate(model, valid_dataloader,
-                            PerceptualLoss_3d.get_loss, device)
+    for epoch in range(training[0].start_epoch, training[0].num_epochs):
+        train_loss = [0.0, 0.0, 0.0]
+        val_loss = [0.0, 0.0, 0.0]
+        for i in range(3):
+            train_loss[i] = train_scans(models[i], train_dataloader, PerceptualLoss_3d.get_loss,
+                            training[i].optimizer, device)
+            val_loss[i] = validate(models[i], valid_dataloader,
+                                PerceptualLoss_3d.get_loss, device)
 
-        scheduler.step(val_loss)
+            training[i].scheduler.step(val_loss[i])
 
-        print(f"Epoch {epoch+1}, Loss: {train_loss}, Val Loss: {val_loss}")
-        logging.info(f"Epoch {epoch+1}, Loss: {train_loss}, "
-                     f"Val Loss: {val_loss} "
-                     f"LR: {scheduler.get_last_lr()}")
+            print(f"Model {i}: Epoch {epoch+1}, Loss: {train_loss}, Val Loss: {val_loss}")
+            logging.info(f"Model {i}: Epoch {epoch+1}, Loss: {train_loss}, "
+                        f"Val Loss: {val_loss} "
+                        f"LR: {training[i].scheduler.get_last_lr()}")
 
-        tensorboard_write(writer, device, train_loss, val_loss, epoch+1,
-                          model, valid_dataloader,
-                          num_images=args.batch_size,
-                          adam_optim=optimizer)
+            tensorboard_write(writer, device, train_loss, val_loss, epoch+1,
+                            models[i], valid_dataloader,
+                            num_images=args.batch_size,
+                            adam_optim=training[i].optimizer)
 
-        # save model checkpoint
-        if args.early_stopping:
-            if best_val_loss - val_loss > min_delta:
-                best_val_loss = val_loss
-                epochs_no_improve = 0
+            # save model checkpoint
+            if args.early_stopping:
+                if best_val_loss - val_loss > min_delta:
+                    best_val_loss = val_loss
+                    epochs_no_improve = 0
 
+                    torch.save({
+                        'epoch': epoch+1,
+                        'model_state_dict': models[i].state_dict(),
+                        'optimizer_state_dict': training[i].optimizer.state_dict()
+                    }, "model_checkpoint.pth")
+
+                else:
+                    epochs_no_improve += 1
+
+                if epochs_no_improve == patience:
+                    print("Early stopping")
+                    logging.info(f'Early stopping at epoch {epoch+1}')
+                    break
+            else:
                 torch.save({
                     'epoch': epoch+1,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict()
+                    'model_state_dict': models[i].state_dict(),
+                    'optimizer_state_dict': training[i].optimizer.state_dict()
                 }, "model_checkpoint.pth")
-
-            else:
-                epochs_no_improve += 1
-
-            if epochs_no_improve == patience:
-                print("Early stopping")
-                logging.info(f'Early stopping at epoch {epoch+1}')
-                break
-        else:
-            torch.save({
-                'epoch': epoch+1,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict()
-            }, "model_checkpoint.pth")
